963b54682eb05ba32af80efab290e28942248c53
Satoshi Takei <s.takei.dev@gmail.com>
2023-04-23 14:54:45 +0900

    Add parsing list and quote

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,9 +1,10 @@
 module Parser (parse) where
 import Text.ParserCombinators.Parsec (
     Parser, oneOf, skipMany1, space, char,
-    many, noneOf, letter, (<|>), digit, many1) 
+    many, noneOf, letter, (<|>), digit, many1,
+    sepBy, endBy, try) 
 import qualified Text.ParserCombinators.Parsec as Parsec (parse)
-import Lisp (LispVal (LvString, LvBool, LvAtom, LvNumber))
+import Lisp (LispVal (LvString, LvBool, LvAtom, LvNumber, LvList, LvDottedList))
 import Control.Monad ( liftM )
 import qualified Text.Parsec.Error
 
@@ -38,8 +39,31 @@ parseNumber :: Parser LispVal
  -- TODO fmap better?
 parseNumber = liftM (LvNumber . read) $ many1 digit
 
+parseList :: Parser LispVal
+parseList = liftM LvList $ parseExpr `sepBy` spaces2
+
+parseDottedList :: Parser LispVal
+parseDottedList = do
+  butlast <- parseExpr `endBy` spaces2
+  tail <- char '.' >> spaces2 >> parseExpr
+  return $ LvDottedList butlast tail
+
+parseQuoted :: Parser LispVal
+parseQuoted = do
+  char '\''
+  expr <- parseExpr
+  return $ LvList [LvAtom "quote", expr]
+
 parseExpr :: Parser LispVal
-parseExpr = parseAtom <|> parseString <|> parseNumber
+parseExpr = 
+  parseAtom 
+  <|> parseString 
+  <|> parseNumber
+  <|> parseQuoted
+  <|> do char '('
+         list <- try parseList <|> parseDottedList
+         char ')'
+         return list
 
 -- TODO Don't use ParseError
 parse :: String -> Either Text.Parsec.Error.ParseError LispVal
