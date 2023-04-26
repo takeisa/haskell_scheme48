b559370db97840194e39feb0e858d3a38f508083
Satoshi Takei <s.takei.dev@gmail.com>
2023-04-26 21:50:58 +0900

    Add eval function

--- src/Eval.hs
+++ src/Eval.hs
@@ -6,3 +6,23 @@ eval val@(LvString _) = val
 eval val@(LvNumber _) = val
 eval val@(LvBool _) = val
 eval (LvList [LvAtom "quote", val]) = val
+eval (LvList (LvAtom func: args)) =
+    apply func $ map eval args
+
+apply :: String -> [LispVal] -> LispVal
+-- maybe :: b -> (a -> b) -> Maybe a -> b
+apply func args = maybe (LvBool False) ($ args) $ lookup func primitives
+
+primitives :: [(String, [LispVal] -> LispVal)]
+primitives = [("+", numericBinOp (+)),
+              ("-", numericBinOp (-)),
+              ("*", numericBinOp (*)),
+              ("/", numericBinOp div)]
+
+numericBinOp :: (Integer -> Integer -> Integer) -> [LispVal] -> LispVal
+numericBinOp op args = LvNumber $ foldl1 op $ map unpackNum args
+
+unpackNum :: LispVal -> Integer
+unpackNum (LvNumber n) = n
+-- TODO error
+unpackNum _ = 0
